dd82b12363fb08cdc06a45d68d2b450db724ab16
tasneem200 <95435667+tasneem200@users.noreply.github.com>
2024-09-05 16:17:36 -0400

    updates

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const images = document.querySelectorAll(".pic");
 let curr = null;
 
 
+images.forEach((pic) => {
+    pic.classList.add("notenlarged");
+});
+
+
 images.forEach((photo) => {
     
-    photo.classList.add("notenlarged");
 
     photo.dataset.originalHeight = photo.height;
     photo.dataset.originalWidth = photo.width;
@@ -17,24 +21,24 @@ images.forEach((photo) => {
         // check if another image is currently enlarged 
         if (curr && curr !== photo){
             // resize current image and set curr to null
-            curr.style.height = `${curr.dataset.originalHeight}px`;
-            curr.style.width = `${curr.dataset.originalWidth}px`;
+            curr.style.height = `${curr.dataset.originalHeight}`;
+            curr.style.width = `${curr.dataset.originalWidth}`;
             curr.classList.remove("enlarged");
             curr.classList.add("notenlarged");
             curr = null;
         }
 
         if (photo.classList.contains("notenlarged")){
-            photo.style.height = `${photo.dataset.originalHeight*1.5}px`;
-            photo.style.width = `${photo.dataset.originalWidth*1.5}px`;
+            photo.style.height = `${photo.dataset.originalHeight*1.5}`;
+            photo.style.width = `${photo.dataset.originalWidth*1.5}`;
             photo.classList.add("enlarged");
             photo.classList.remove("notenlarged");
             curr = photo;
         }
         else{
             console.log("shrinking");
-            photo.style.height = `${photo.dataset.originalHeight}px`;
-            photo.style.width = `${photo.dataset.originalWidth}px`;
+            photo.style.height = `${photo.dataset.originalHeight}`;
+            photo.style.width = `${photo.dataset.originalWidth}`;
             photo.classList.remove("enlarged");
             photo.classList.add("notenlarged");
         }
